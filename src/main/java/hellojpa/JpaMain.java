085e8db5208aee85f81e04ab053544d47143524d
Gatsjy <Gatsjy@DESKTOP-4TT9522>
2020-10-18 21:53:57 +0900

    2020-10-18, 한주안, 플러쉬

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -25,11 +25,18 @@ public class JpaMain {
          *  - 엔티티 매니저 팩토리는 하나만 생성해서 애플리케이션 전체에서 공유
          *  - 엔티티 매니저는 쓰레드간 공유X(사용하고 버려야 한다.)
          *  - JPA의 모든 데이터 변경은 트랜잭션 안에서 실행해야 한다.
+         *  
+         *  영속성 컨텍스트를 플러시하는 방법
+         *  - em.flush() - 직접 호출
+         *  - 트랜잭션 커밋 - 플러시 자동 호출
+         *  - JPQL 쿼리 실행 - 플러시 자동 호출
          */
         try{
             // 영속 엔티티 조회
-            Member member = em.find(Member.class, 150L);
-            member.setName("ZZZZZ"); // 객체를 컬렉션처럼 다루기 위해서 jpa를 사용한다는 점을 인지하자
+            Member member = new Member(200L, "member200");
+            em.persist(member);
+
+            em.flush();
 
             System.out.println("=======================");
             tx.commit();
