0d9c51b98376a8365263d6d2a2646190690c79d4
Gatsjy <gkswndks123@naver.com>
2020-10-20 21:18:23 +0900

    2020-10-20, 한주안, 필드 값 구현

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,7 +1,7 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 /**
  * Created by Gatsjy on 2020-10-18
@@ -14,7 +14,23 @@ public class Member {
 
     @Id
     private Long id;
+
+    @Column(name = "name")
     private String name;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description;
 
     protected Member(){
     }
---
+++ src/main/java/hellojpa/RoleType.java
@@ -0,0 +1,11 @@
+package hellojpa;
+
+/**
+ * @author Gatsjy
+ * @since 2020-10-20
+ * realize dreams myself
+ * Blog : https://blog.naver.com/gkswndks123
+ * Github : https://github.com/gatsjy
+ */
+public class RoleType {
+}
